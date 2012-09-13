a9ecfc7cc0b2b21cf1e516f85417fd67ffd4c5d3
babaru <babaru.trinit@gmail.com>
2012-09-13 12:23:21 +0800

    today

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,64 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+default_run_options[:pty] = true
+set :application, "Hati"
+set :repository,  "git@bitbucket.org:babaru/hati.git"
+set :domain, "106.187.93.197"
 
-set :scm, :subversion
+set :rvm_ruby_string, '1.9.3-head'
+require "rvm/capistrano"
+set :rvm_type, :system
+
+set :scm, :git
+set :scm_passphrase, "Mbi800716"
+
+set :user, "app"
+set :password, "Mbi800716"
+
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+ssh_options[:forward_agent] = true
+set :branch, "master"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :deploy_to, "~/www_root/hati.tfocusclub.com/app/hati"
+
+# load 'deploy/assets'
+
+set :use_sudo, false
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  
+  task :start, :roles => :app, :except => { :no_release => true } do 
+	invoke_command "cd #{current_path};./script/ferret_server -e production start"
+    invoke_command "service thin start"
+  end
+
+  task :stop do
+  	invoke_command "cd #{current_path};./script/ferret_server -e production stop"
+    invoke_command "service thin stop"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    invoke_command "cd #{current_path};./script/ferret_server -e production stop"
+    invoke_command "service thin stop"
+
+    invoke_command "cd #{current_path};./script/ferret_server -e production start"
+    invoke_command "service thin start"
+  end
+  
+  task :seed, :roles => :app do
+    run "cd #{current_path} && /usr/bin/env rake db:seed RAILS_ENV=production"
+  end
+
+end
+
+require 'bundler/capistrano'
+
+after "deploy:update_code", "deploy:migrate"
+after "deploy:migrate", "deploy:seed"
+after "deploy:create_symlink", "deploy:restart"
