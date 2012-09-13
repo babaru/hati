57403a174e5ebafd0db5e9e852c412f9d5fa01d8
babaru <babaru.trinit@gmail.com>
2012-09-13 16:58:13 +0800

    today

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,4 +55,4 @@ require 'bundler/capistrano'
 
 after "deploy:update_code", "deploy:migrate"
 # after "deploy:migrate", "deploy:seed"
-# after "deploy:create_symlink", "deploy:restart"
+after "deploy:create_symlink", "deploy:restart"
