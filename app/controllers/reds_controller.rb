ad3f719b532c2e6fce46e3485fc44356dd234e20
babaru <babaru.trinit@gmail.com>
2012-09-18 22:01:54 +0800

    today

--- app/controllers/reds_controller.rb
+++ app/controllers/reds_controller.rb
@@ -1,6 +1,12 @@
 class RedsController < ApplicationController
   def index
-    Rails.logger.info "Request header: \r\n"
+    Rails.logger.info "Request /index header: \r\n"
+    Rails.logger.info request.env
+    redirect_to "http://hati.tfocusclub.com/reds/sec"
+  end
+
+  def sec
+    Rails.logger.info "Request /sec header: \r\n"
     Rails.logger.info request.env
     render :nothing => true
   end
