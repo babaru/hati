73ded82cfff9bb51ff4eb885b9b5cff9377ba138
babaru <babaru.trinit@gmail.com>
2012-09-18 16:50:39 +0800

    today

--- app/controllers/reds_controller.rb
+++ app/controllers/reds_controller.rb
@@ -2,23 +2,5 @@ class RedsController < ApplicationController
   def index
     Rails.logger.debug "Request header: \r\n"
     Rails.logger.debug request.env
-
-    # uri = URI.parse("http://hati.dev/reds/second_index")
-    # http = Net::HTTP.new(uri.host, uri.port)
-    # headers = {
-    #   'REFERER' => "weibo.com",
-    #   'Status' => '302'
-    # }
-    # http.get(uri.path, headers)
-    # render :nothing => true
-
-    headers["HTTP_REFERER"] = "http://old.url"
-    redirect_to "http://hati.dev/reds/second_index"
-  end
-
-  def second_index
-    Rails.logger.debug "Request header: \r\n"
-    Rails.logger.debug request.env
-    render :nothing => true
   end
 end
