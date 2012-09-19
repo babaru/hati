c549fe8725438fb298cc98faa2a039d9bd114720
babaru <babaru.trinit@gmail.com>
2012-09-19 15:44:23 +0800

    today

--- app/controllers/reds_controller.rb
+++ app/controllers/reds_controller.rb
@@ -2,11 +2,14 @@ class RedsController < ApplicationController
   layout 'reds'
 
   def index
+    logger.debug request.env
+
     reds = Reds.find_by_code params[:code]
     render and return if reds.nil?
 
     Click.create header:request.env.to_s, referal:request.env["HTTP_REFERER"], remote_ip:request.env["REMOTE_ADDR"], reds_id:reds.id
+    @url = reds.url
 
-    redirect_to reds.url
+    # redirect_to reds.url
   end
 end
