f4392729d1fc4d404a8173cee586e8a4f7152f85
babaru <babaru.trinit@gmail.com>
2012-10-18 17:35:45 +0800

    today

--- app/controllers/reds_controller.rb
+++ app/controllers/reds_controller.rb
@@ -7,7 +7,7 @@ class RedsController < ApplicationController
     reds = Reds.find_by_code params[:code]
     render and return if reds.nil?
 
-    Click.create header:request.env.to_s, referal:request.env["HTTP_REFERER"], remote_ip:request.env["REMOTE_ADDR"], reds_id:reds.id, real_ip:request.env["HTTP_X_REAL_IP"], forwarded_for:request.env["HTTP_X_FORWARDED_FOR"]
+    # Click.create header:request.env.to_s, referal:request.env["HTTP_REFERER"], remote_ip:request.env["REMOTE_ADDR"], reds_id:reds.id, real_ip:request.env["HTTP_X_REAL_IP"], forwarded_for:request.env["HTTP_X_FORWARDED_FOR"]
     @url = reds.url
   end
 
