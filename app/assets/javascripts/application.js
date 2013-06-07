d4d12fd8ab8f5647f91d25417d243f55e325b0af
babaru <babaru.trinit@gmail.com>
2013-06-07 16:21:40 +0800

    today

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,19 +13,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
-//= require jquery-ui-timepicker-addon
-//= require jquery.remotipart
-//= require twitter/bootstrap
-//= require load-image.min
-//= require bootstrap-image-gallery.min
+//= require bootstrap
 //= require_tree .
 
-$(document).ready(function() {
-  $('.dropdown-toggle').dropdown();
-  $('input.datepicker').datepicker({dateFormat: 'yy-mm-dd'});
-  $('input.datetimepicker').datetimepicker({dateFormat: 'yy-mm-dd', timeFormat: 'hh:mm'});
+// $(document).ready(function() {
+//   // $('.dropdown-toggle').dropdown();
+//   // $('input.datepicker').datepicker({dateFormat: 'yy-mm-dd'});
+//   // $('input.datetimepicker').datetimepicker({dateFormat: 'yy-mm-dd', timeFormat: 'hh:mm'});
 
-  $('a[tooltip="true"]').tooltip();
+//   // $('a[tooltip="true"]').tooltip();
 
-  $('.loading-icon').hide();
-});
+//   // $('.loading-icon').hide();
+// });
